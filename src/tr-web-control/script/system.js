2e9b26b6942ce67427de9b115ffae8bc6f6aa4d6
kasru <kasru@users.noreply.github.com>
2026-09-18 12:03:07 +0300

    chore(version): bump Web Control to 1.6.1-fork1 (codeupdate 20260918)
    Marks this fork's build (Transmission 4.x RPC compatibility layer) distinctly
    in the About dialog: 'Web Control: 1.6.1-fork1 (20260918)'.
    Regenerates system.min.js and bumps its cache-buster.

--- src/tr-web-control/script/system.js
+++ src/tr-web-control/script/system.js
@@ -1,8 +1,8 @@
 // Current system global object
 var system = {
-	version: "1.6.1",
+	version: "1.6.1-fork1",
 	rootPath: "tr-web-control/",
-	codeupdate: "20200913",
+	codeupdate: "20260918",
 	configHead: "transmission-web-control",
 	// default config, can be customized in config.js
 	config: {
